9ccc09b64889ef15c0fbd91317f5b47ce969dd5f
Indresh Goswami <indreshgoswami149@gmail.com>
2022-02-15 00:56:31 +0530

    Added solution - LeetHub

---
+++ Anagram of String - GFG/anagram-of-string.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int CHARS = 26;
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2);
+
+// Driver program to run the case
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str1,str2;
+        cin>>str1>>str2;
+        cout << remAnagram(str1, str2);
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2)
+{
+// Your code goes here
+     vector<int> f(26);
+     for(char c: str1) f[c-'a']++;
+     for(char c: str2) f[c-'a']--;
+     int sum = 0;
+     for(int x: f) sum+=abs(x);
+     return sum;
+
+}
