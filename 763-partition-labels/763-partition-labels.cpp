e88dc5d8ea9293b30fa5fbe2d348c5dc67b252f0
Indresh Goswami <indreshgoswami149@gmail.com>
2022-03-21 12:09:15 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (96.62%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> idx(26,0);
+        for(int i=0;i<s.length();i++)
+        {
+            idx[s[i] - 'a'] = i;
+        }
+        vector<int> res;
+        int start =0,end=0;
+        for(int i=0;i<s.length();i++)
+        {
+            end = max(end,idx[s[i] - 'a']);
+            if(i == end)
+            {
+                res.push_back(i-start+1);
+                start = i+1;
+            }
+        }
+        return res;
+    }
+};
