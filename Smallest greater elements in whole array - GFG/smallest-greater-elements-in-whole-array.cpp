b9b06f7c2401995a479bfdf931ecc5ec031fb192
Indresh Goswami <indreshgoswami149@gmail.com>
2022-03-25 01:22:55 +0530

    Added solution - LeetHub

---
+++ Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int* greaterElement(int arr[], int n);
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int *b;
+        b = greaterElement(arr, n);
+        for(int i=0;i<n;i++){
+            if(b[i] == -10000000)
+                cout << "_ ";
+            else
+                cout << b[i] << " ";
+        }
+        cout<<endl;   
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+int* greaterElement(int arr[], int n)
+{
+    // Complete the function
+    set<int>s;
+    for(int i=0;i<n;i++){
+        s.insert(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        auto it=s.find(arr[i]);
+        if(*it==*s.rbegin()){
+            arr[i]=-10000000;
+        }
+        else{
+            it++;
+            arr[i]=*it;
+        }
+    }
+    int *b;
+    b=arr;
+    return b;
+}
