1ff9a4a6a0190867c3c13f082d08311912c893d9
Indresh Goswami <indreshgoswami149@gmail.com>
2022-04-18 16:21:10 +0530

    Added solution - LeetHub

---
+++ K-Ary Tree - GFG/kary-tree.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+#define ll long long 
+const int mod = 1e9+7;
+
+ll expo(ll base,ll pow )
+{
+    ll ans=1;
+    while(pow)
+    {
+        if(pow&1)
+           ans = (ans *1ll* base)%mod; 
+
+        base = (base * 1ll *base) %mod;
+        pow = pow>>1;
+    }
+    return ans;
+
+}
+
+class Solution {
+  public:
+    long long karyTree(int k, int m) {
+        // code here
+         return expo(k,m);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k,m;
+        
+        cin>>k>>m;
+
+        Solution ob;
+        cout << ob.karyTree(k,m) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
