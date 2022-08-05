cf2a8f825964019a292c5d7becedd0c917c7a0db
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-05 20:31:40 +0530

    Time: 3 ms (62.90%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int f(vector<int> &num, int tar,vector<int> &dp){
-        if(tar < 0) return 0;
-        
-        if(tar == 0) return 1;
-        
-        if(dp[tar] != -1) return dp[tar];
-        int ans = 0;
-        for(int i=0;i<num.size();i++){
-            ans += f(num,tar - num[i],dp);
-        }
-        return dp[tar] = ans;
-    }
+    
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
-       return f(nums,target,dp); 
+        vector<uint> dp(target+1,0);
+        dp[0] = 1;
+        
+        //traversing from target 1  to target
+        for(int i=1;i<=target;i++){
+            
+            //traversing on num vector
+            for(int j=0;j<nums.size();j++){
+                if(i - nums[j] >= 0)
+                  dp[i] += dp[i - nums[j]];
+            }
+        }
+        
+       return dp[target]; 
     }
 };
