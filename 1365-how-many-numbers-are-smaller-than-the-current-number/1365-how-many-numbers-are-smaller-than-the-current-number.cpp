b5347da6a9c802cb15fa410892d7959e832c9674
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-15 20:25:40 +0530

    Time: 15 ms (68.89%), Space: 10.5 MB (13.41%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        map<int,int> mp;
+        map<int,int> mp;  // Map for storing frequencies of number less than that number
         int n = nums.size();
         
-        vector<int> snum = nums;
-        sort(snum.begin(),snum.end());
+        vector<int> sortednum = nums;
+        sort(sortednum.begin(),sortednum.end());
+        
+        //Iterating from back and assigning index of number to that number 
         for(int i = n-1;i>=0;i--)
         {
-            mp[snum[i]] = i;
+            mp[sortednum[i]] = i;
         }
+        
+        //Assigning frequencies to each elements
         for(int i= 0;i<n;i++){
             nums[i] = mp[nums[i]];
         }
