96b8d17781fd3315be94384b7a4b411b4bd137e7
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-19 12:16:20 +0530

    Time: 56 ms (50.86%), Space: 13.4 MB (26.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
-    void bfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &grid,vector<int> & dr,vector<int> &dc)
-  {
-      vis[row][col] = 1;
-      queue<pair<int,int>> q;
-      q.push({row,col});
-      int n = grid.size();
-      int m = grid[0].size();
-      
-      while(!q.empty()){
-          int row = q.front().first;
-          int col = q.front().second;
-          q.pop();
-          
-          //traverse in the neighbours and mark them if its a land
-          for(int i=0;i<4;i++)
+     void dfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> & visited,vector<int> & dr,vector<int> &dc)
+    {
+        visited[row][col] = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i=0;i<4;i++)
         {
-                  int nrow=row+dr[i],ncol=col+dc[i];
-                  if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && !vis[nrow][ncol])
-                  {
-                      vis[nrow][ncol] = 1;
-                      q.push({nrow,ncol});
-                  }
+          int nrow=row+dr[i],ncol=col+dc[i];
+                
+              if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visited[nrow][ncol])  
+              { 
+                  dfs(nrow,ncol,grid,visited,dr,dc);
+              }  
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+      int m=grid[0].size();  
+      vector<vector<int>> visited(n,vector<int>(m,0));
+      int cnt=0;  
+      vector<int> dr,dc;
+      dr={-1,1,0,0};
+       dc={0,0,-1,1}; 
+      for(int row =0;row<n;row++)
+      {
+          for(int col=0;col<m;col++)
+          {
+              if(!visited[row][col] && grid[row][col]=='1')
+              {
+                  cnt++;
+                  dfs(row,col,grid,visited,dr,dc);
               }
           }
       }
-  
-    
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int cnt = 0;
-        vector<int> dr,dc;
-        dr={-1,1,0,0};
-        dc={0,0,-1,1}; 
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int row = 0; row < n;row++){
-            for(int col = 0;col < m;col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
-                    cnt++;
-                    bfs(row,col,vis,grid,dr,dc);
-                }
-            }
-        }
-        return cnt;
+    return cnt;    
     }
 };
