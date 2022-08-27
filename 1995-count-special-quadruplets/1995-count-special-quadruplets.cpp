a2822bf38204f83fca5f7b28f8de7f5c6edc98f9
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-27 15:29:44 +0530

    Time: 244 ms (39.14%), Space: 10.5 MB (82.32%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int countQuadruplets(vector<int>& nums) {
           int res = 0, n = nums.size();
-    for (int i = 0; i < n; ++i)
-        for (int j = i + 1; j < n; ++j)
-            for (int k = j + 1; k < n; ++k)
+    for (int i = 0; i < n-3; ++i)
+        for (int j = i + 1; j < n-2; ++j)
+            for (int k = j + 1; k < n-1; ++k)
                 for (int l = k + 1; l < n; ++l)
                     res += nums[i] + nums[j] + nums[k] == nums[l];
     return res;     
