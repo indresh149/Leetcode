91bf069730d2ac195e1dff3daeb7df270b35a036
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-29 14:03:59 +0530

    Time: 33 ms (70.26%), Space: 32.3 MB (8.73%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+       int n=nums.size();
+       vector<int> pref(n);
+       vector<int> suffix(n);
+        
+        int suma=0;
+        for(int i=0;i<n;i++){
+            pref[i]=suma;
+            suma=suma+nums[i];
+        }
+       
+        int sumb=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            suffix[i]=sumb;
+            sumb=sumb+nums[i];
+        }
+      
+        
+        int pos=-1;
+        
+        for(int i=0;i<n;i++){
+            if(pref[i]==suffix[i])
+            {
+                pos=i;
+                break;
+            }
+        }
+        return pos;
+    }
+};
