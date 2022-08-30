fbecef9020e1ed810bae2645cba6ecc04f23e7a8
Indresh Goswami <indreshgoswami149@gmail.com>
2022-08-30 14:10:16 +0530

    Time: 3 ms (51.27%), Space: 8 MB (7.53%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
+   bool isSubsequence(string s, string t) {
     
-    // returns the length of longest common subsequence
-    int isSubs(string& s1, string& s2, int i , int j,vector<vector<int>> &t)
-    {
-        if(i == 0 || j == 0)
-            return 0;
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s1[i-1] == s2[j-1])
-            return t[i][j] = 1 + isSubs(s1,s2,i-1,j-1,t);
-        else
-            return t[i][j] = isSubs(s1,s2,i,j-1,t);
+    int m=s.length();
+    int n=t.length();
+    vector<vector<int>>dp(m+1,vector<int>(n+1));
+    
+    for(int i=1;i<=m;i++)dp[i][0]=0;
+    for(int j=0;j<=n;j++)dp[0][j]=1;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1];
+            else dp[i][j]=dp[i][j-1];
+        }
     }
+    return dp[m][n];
     
-    
-    bool isSubsequence(string s1, string s2) {
-        int m = s1.length();
-        int n = s2.length();
-        
-        if(m >  n)
-            return false;
-        
-        vector<vector<int>> t(m+1,vector<int> (n+1,-1));
-    
-        if(isSubs(s1,s2,m,n,t) == m)
-            return true;
-        return false;
-    }
+}
 };
