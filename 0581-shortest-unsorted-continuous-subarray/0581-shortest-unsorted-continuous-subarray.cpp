c8b2535dfd20a59f0ebb96ef14eebb59e669c555
Indresh Goswami <indreshgoswami149@gmail.com>
2023-01-08 01:55:06 +0530

    Time: 39 ms (72.97%), Space: 26.6 MB (73.56%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-      int low = 0,high = nums.size()-1;
-        while(low+1 < nums.size() && nums[low] <= nums[low+1]) low++;
-        while(high-1 >= 0 && nums[high-1] <= nums[high]) high--;
+        int Min = INT_MAX;
+        int Max= INT_MIN;
+        int n = nums.size();
+        if(n == 1) return 0;
         
-        if(low == nums.size()-1) return 0;
-        
-        int wMin = INT_MAX,wMax = INT_MIN;
-        
-        for(int i= low;i <= high;i++){
-            wMin = min(wMin,nums[i]);
-            wMax = max(wMax,nums[i]);
+        for(int i=0;i<n;i++)
+        {
+            if(i == 0)
+            {
+                if(nums[i] > nums[i+1])
+                {
+                    Min = min(Min,nums[i]);
+                    Max = max(Max,nums[i]);
+                }
+            }
+            else if(i == n-1)
+            {
+                if(nums[i] < nums[i-1])
+                {
+                    Min = min(Min,nums[i]);
+                    Max = max(Max,nums[i]);
+                }
+            }
+            else
+            {
+            if(nums[i] > nums[i+1]  || nums[i] < nums[i-1])
+            {
+                Min = min(Min,nums[i]);
+                Max = max(Max,nums[i]);
+            }
         }
+        }
+        if(Min == INT_MAX || Max == INT_MIN) return 0;
         
-        while(low-1 >= 0 && nums[low-1] > wMin) low--;
-        while(high+1 <= nums.size()-1 && nums[high+1] < wMax) high++;
+        // find the positions of min,max;
+        int i,j;
+        for(i=0;i<n && nums[i] <= Min;i++);
+        for(j = n-1;j >= 0 && nums[j] >= Max;j--);
+        return j-i+1;
+            
         
-        return high-low+1;
     }
 };
